89ead7fa688c39e4b89cdf0a177781c3270f3f83
jerkos <cram@hotmail.fr>
2015-08-11 13:31:22 +0200

    allow guest to see user profile

--- metabomatch/user/views.py
+++ metabomatch/user/views.py
@@ -36,9 +36,9 @@ user = Blueprint("user", __name__, template_folder="templates")
 @user.route("/<username>")
 def profile(username):
     user = User.query.filter_by(username=username).first_or_404()
-    print "GUEST_USER_ID:", GUEST_USER_ID, " user.id:", user.id
-    if user.id == GUEST_USER_ID or username == 'Guest':
-        return render_template('errors/forbidden_page.html')
+    # print "GUEST_USER_ID:", GUEST_USER_ID, " user.id:", user.id
+    # if user.id == GUEST_USER_ID or username == 'Guest':
+    #     return render_template('errors/forbidden_page.html')
     return render_template("user/profile.html", user=user)
 
 
